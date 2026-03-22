ac20ff47285aae50cf94044f149cceddb15a1751
ChhapendraSarki <sarkichhapendra28@gmail.com>
2026-03-22 22:59:49 +0530

    Add files via upload

---
+++ Ascii.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int numberOne;
+
+int main (void)
+{
+				printf("Enter The Number One : ");
+				scanf("%d", &numberOne);
+				
+				printf("%d Is Ascii Value Of %c.\n", numberOne, numberOne);
+				
+				return(0);
+}
