1bfac616df178c5b2bb7090d1b5b6206d03de227
ChhapendraSarki <sarkichhapendra28@gmail.com>
2026-03-22 23:12:23 +0530

    Add files via upload

---
+++ BitwiseAnd.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int numberOne;
+int numberTwo;
+int numberThree;
+
+int main (void)
+{
+				printf("Enter Your First Value : ");
+				scanf("%d", &numberOne);
+				
+				printf("Enter Your Second Value : ");
+				scanf("%d", &numberTwo);
+				
+				numberThree = numberOne && numberTwo;
+				
+				printf("%d && %d = %d\n", numberOne, numberTwo, numberThree);
+				
+				return(0);
+}
